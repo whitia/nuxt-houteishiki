10836aa064c3c8a3c24136c4bbc8c942cccdca7a
whitia <whitia7@gmail.com>
2020-03-12 16:18:43 +0900

    modify title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    //title: process.env.npm_package_name || '',
+    title: 'みんなの方程式',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
