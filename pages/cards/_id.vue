c3419bbbfe942c8b3a638a8fbe4cd4ddfd80b5a5
whitia <whitia7@gmail.com>
2020-03-13 15:10:11 +0900

    modify other cards

--- pages/cards/_id.vue
+++ pages/cards/_id.vue
@@ -33,7 +33,7 @@
           </b-card>
           <div class="row my-5">
             <div class="col-12 mb-3">
-              <h4>{{ this.$store.getters.getUser.name }} の他のカード</h4>
+              <h4>{{ this.card.user.name }} の他のカード</h4>
             </div>
             <div class="col-12 col-sm-4" v-for="(card,key) in $store.getters.getUserCards" v-bind:key="key">
               <nuxt-link :to="{ name: 'cards-id', params: { id: card.id, card: card } }">
@@ -86,7 +86,7 @@ export default {
       this.auth = true;
     }
 
-    this.$store.dispatch('fetchUserCards', { user, limit: max + 1 })
+    this.$store.dispatch('fetchUserCards', { user: card.user, limit: max + 1 })
       .then(() => {
         this.$store.commit('updateUserCards', { card, max })
       })
